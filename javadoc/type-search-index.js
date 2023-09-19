1e2227cb54eefd848df0c753791673f0a2cea641
MacBook <macbook@MacBook-Pro-User.local>
2023-09-20 02:07:50 +0300

    javadoc generated files added

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"App"},{"p":"org.example","l":"AppTest"},{"p":"org.example.model","l":"Item"},{"p":"org.example.model","l":"Person"}];updateSearchResults();
